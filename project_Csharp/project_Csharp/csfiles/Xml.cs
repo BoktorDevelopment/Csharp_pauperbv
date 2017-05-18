0f141b5c1fdf6596fd192403332ff6e027a4e5f8
Kris Broekstra <97042580@st.deltion.nl>
2017-05-18 20:20:02 +0200

    Grootte update Xml & object orientated

--- project_Csharp/project_Csharp/csfiles/Xml.cs
+++ project_Csharp/project_Csharp/csfiles/Xml.cs
@@ -9,11 +9,35 @@ namespace project_Csharp.csfiles
 {
     public class Xml
     {
-        public int readXML()
-        {
-            int var = 10;
+       public DataSet ds = new DataSet("playlist");
 
-            return var;
+        public void getSongs() {
+            
+            DataTable dtSongs = new DataTable("song");
+
+            DataColumn dtID = new DataColumn("id");
+            DataColumn dtArtist = new DataColumn("artist");
+            DataColumn dtTitle = new DataColumn("title");
+            DataColumn dtYear = new DataColumn("year");
+            DataColumn dtGenre = new DataColumn("genre");
+            DataColumn dtTime = new DataColumn("time");
+            DataColumn dtFile = new DataColumn("file");
+
+            ds.Tables.Add(dtSongs);
+            dtSongs.Columns.Add(dtID);
+            dtSongs.Columns.Add(dtArtist);
+            dtSongs.Columns.Add(dtTitle);
+            dtSongs.Columns.Add(dtYear);
+            dtSongs.Columns.Add(dtGenre);
+            dtSongs.Columns.Add(dtTime);
+            dtSongs.Columns.Add(dtFile);
+
+            ds.ReadXml(System.Web.HttpContext.Current.Server.MapPath("data/playlist.xml"));
+        }
+
+
+        public void addsong()
+        {
         }
           
     }
