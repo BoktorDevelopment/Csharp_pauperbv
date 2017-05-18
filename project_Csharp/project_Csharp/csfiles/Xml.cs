68404254a4244cb8295bb393e1437a4db49c1ced
Kris Broekstra <97042580@st.deltion.nl>
2017-05-18 20:52:26 +0200

    update voor de add

--- project_Csharp/project_Csharp/csfiles/Xml.cs
+++ project_Csharp/project_Csharp/csfiles/Xml.cs
@@ -11,17 +11,20 @@ namespace project_Csharp.csfiles
     {
        public DataSet ds = new DataSet("playlist");
 
+        DataTable dtSongs = new DataTable("song");
+        DataColumn dtID = new DataColumn("id");
+        DataColumn dtArtist = new DataColumn("artist");
+        DataColumn dtTitle = new DataColumn("title");
+        DataColumn dtYear = new DataColumn("year");
+        DataColumn dtGenre = new DataColumn("genre");
+        DataColumn dtTime = new DataColumn("time");
+        DataColumn dtFile = new DataColumn("file");
+
         public void getSongs() {
             
-            DataTable dtSongs = new DataTable("song");
+            
 
-            DataColumn dtID = new DataColumn("id");
-            DataColumn dtArtist = new DataColumn("artist");
-            DataColumn dtTitle = new DataColumn("title");
-            DataColumn dtYear = new DataColumn("year");
-            DataColumn dtGenre = new DataColumn("genre");
-            DataColumn dtTime = new DataColumn("time");
-            DataColumn dtFile = new DataColumn("file");
+          
 
             ds.Tables.Add(dtSongs);
             dtSongs.Columns.Add(dtID);
@@ -38,6 +41,30 @@ namespace project_Csharp.csfiles
 
         public void addsong()
         {
+            if (HttpContext.Current.Request.HttpMethod == "POST") 
+            {
+                string artist = HttpContext.Current.Request.Form["artist"];
+                string title = HttpContext.Current.Request.Form["title"];
+                string year = HttpContext.Current.Request.Form["year"];
+                string genre = HttpContext.Current.Request.Form["genre"];
+                string time = HttpContext.Current.Request.Form["time"];
+                string file = HttpContext.Current.Request.Form["file"];
+
+                DataRow dr = ds.Tables[0].NewRow();
+
+                dr["artist"] = artist;
+                dr["title"] = title;
+                dr["year"] = year;
+                dr["genre"] = genre;
+                dr["time"] = time;
+                dr["file"] = file;
+
+                ds.Tables[0].Rows.Add(dr);
+
+                ds.WriteXml(System.Web.HttpContext.Current.Server.MapPath("data/playlist.xml"));
+            }
+
+
         }
           
     }
