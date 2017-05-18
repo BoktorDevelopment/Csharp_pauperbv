fc9c343283e5c7f83d9ddeda8e8bc4dc6590942e
BoktorDevelopment <kris.broekstra@outlook.com>
2017-05-18 11:17:21 +0200

    xml update #2

--- project_Csharp/project_Csharp/csfiles/Xml.cs
+++ project_Csharp/project_Csharp/csfiles/Xml.cs
@@ -3,35 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data;
+using System.Xml;
 
 namespace project_Csharp.csfiles
 {
     public class Xml
     {
-
-        public void createXML()
-        {
-            DataSet ds = new DataSet("playlist");
-            DataTable dtSongs = new DataTable("song");
-
-            DataColumn dtID = new DataColumn("id");
-            DataColumn dtArtist = new DataColumn("artist");
-            DataColumn dtTitle = new DataColumn("title");
-            DataColumn dtYear = new DataColumn("year");
-            DataColumn dtGenre = new DataColumn("genre");
-            DataColumn dtTime = new DataColumn("time");
-            DataColumn dtFile = new DataColumn("file");
-            
-            ds.Tables.Add(dtSongs);
-            dtSongs.Columns.Add(dtID);
-            dtSongs.Columns.Add(dtArtist);
-            dtSongs.Columns.Add(dtTitle);
-            dtSongs.Columns.Add(dtYear);
-            dtSongs.Columns.Add(dtGenre);
-            dtSongs.Columns.Add(dtTime);
-            dtSongs.Columns.Add(dtFile);
-
-            ds.ReadXml(Server.MapPath("data/playlist"));
+        public void readXML() {
+            XmlTextReader playlist = new XmlTextReader("data/playlist.xml");
+            while (playlist.Read())
+            {
+                // Gegevens hier verwerken.
+                Console.WriteLine(playlist.Name);
+            }
+            Console.ReadLine();
         }
     }
 }
